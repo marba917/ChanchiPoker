1ab043d3c0199ffae77ebf90b4052cba03f1f402
Clara Ines Jaramillo <pachajaramillo@gmail.com>
2016-02-15 17:15:56 -0500

    Initial Commit

---
+++ ChanchiPoker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChanchiPoker
+//
+//  Created by TAKTIL INC on 2/15/16.
+//  Copyright © 2016 TAKTIL INC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChanchiPoker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChanchiPoker
+//
+//  Created by TAKTIL INC on 2/15/16.
+//  Copyright © 2016 TAKTIL INC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
